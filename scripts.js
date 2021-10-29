9206a6ed86bbf90e77be263cb30e8eea44bc9692
TYLPHE <tylphet@gmail.com>
2021-10-29 00:02:34 -0600

    add event listener to update array and board

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 // (function () {
     let gameBoard = {
         array: [
-            `x`, `x`, `x`,
-            `x`, `x`, `x`,
-            `x`, `x`, `o`,
+            ``, ``, ``,
+            ``, ``, ``,
+            ``, ``, ``,
         ],
         init: function(){
             this.domDefine();
@@ -13,21 +13,48 @@
             this.$board = document.querySelector(`.game-board`);
             this.$boardChild = document.querySelector(`.game-board-child`);
         },
-        render: function(){
-            let data = this.array;
-            this.$boardChild.textContent = ``;
-            for(let i = 0; i < data.length; i++){
-                this.$boardChild.textContent += `${data[i]}, `;
-            };
-        },
         renderGrid: function(){
             for(i = 0; i < this.array.length; i++){
                 let grid = document.createElement(`div`);
                 this.$board.appendChild(grid);
                 grid.className = `grid ${i}`;
-                grid.textContent = i;
-            }
+                grid.id = i;
+                grid.textContent = this.array[i];
+            };
+            gameBoard.addListeners();
+        },
+        wipeGrid: function(){
+            for(i = 0; i < this.array.length; i++){
+                document.getElementById(i).remove();
+            };
+        },
+        addListeners: function(){
+            for(i = 0; i < this.array.length; i++){
+                document.getElementById(i).addEventListener(`click`, this.markBoard);
+            };
+        },
+        markBoard: function(){
+            // for(const num of this.array){
+            //     this.array[num]
+            // }
+            gameBoard.array[this.id] = player1.getShape;
+            gameBoard.wipeGrid();
+            gameBoard.renderGrid();
+        },
+        roundCounter: function(){
+            let round = 0;
+            
         }
     };
+    const player = function(name, shape){
+        const getShape = shape;
+        const getName = name;
+        const selectGrid = function(){
+            console.log(`hello`);
+        };
+        return {selectGrid, getShape, getName}
+    };
+    const player1 = player(`player 1`, `X`);
+    const player2 = player(`Player 2`, `O`);
     gameBoard.init();
 // })();
